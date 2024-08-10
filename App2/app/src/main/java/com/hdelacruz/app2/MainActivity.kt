73388f3141ce793af521b3ca8160495203d8fef8
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2024-08-09 19:46:18 -0600

    App 2 finalizado

--- App2/app/src/main/java/com/hdelacruz/app2/MainActivity.kt
+++ App2/app/src/main/java/com/hdelacruz/app2/MainActivity.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.hdelacruz.app2
 
@@ -72,6 +58,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hdelacruz.app2.ui.theme.MySootheTheme
+
+/**
+ * Autor: Humberto de la Cruz
+ * Fecha: 09/08/2024
+ * App 2
+ */
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
