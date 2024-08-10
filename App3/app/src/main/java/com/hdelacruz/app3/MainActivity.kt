fe4c476d2c13deeb809cc68fcdfcccb0c7cb0760
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2024-08-09 18:21:15 -0600

    Se finalizó el programa app3

---
+++ App3/app/src/main/java/com/hdelacruz/app3/MainActivity.kt
@@ -0,0 +1,32 @@
+
+package com.hdelacruz.app3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.hdelacruz.app3.ui.theme.BasicStateCodelabTheme
+/*
+* Autor: Humberto de la Cruz
+* Fecha: 09/08/2024
+* APP 3
+* */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicStateCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
